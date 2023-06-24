8324fe88dd5f1671122847e9f1553ea4192a95d0
Elinsky <dmitry.ilinsky@gmail.com>
2023-06-24 18:08:52 +0300

    All five tests are passed.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
+
+    public static String dbUrl = "jdbc:mysql://192.168.89.21:3306/firstdb";
+    public static String dbUsername = "root";
+    public static String dbPassword = "root";
     // реализуйте настройку соеденения с БД
+    private static Connection connection;
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            System.out.println("\nConnection established\n");
+        } catch (SQLException e) {
+            throw new RuntimeException("Database properties file not available or no connection ", e);
+        }
+        return connection;
+    }
 }
